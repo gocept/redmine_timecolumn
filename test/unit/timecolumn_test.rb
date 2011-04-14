f1493a68885686ff8498c2c4aa80de5e07c5839e
Wolfgang Schnerring <wosc@wosc.de>
2011-04-14 14:52:05 +0200

    Add test for spent time column

--- test/unit/timecolumn_test.rb
+++ test/unit/timecolumn_test.rb
@@ -2,10 +2,15 @@
 # See also LICENSE.txt
 
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+require 'time_column'
 
 
 class TimeColumnTest < ActiveSupport::TestCase
 
   fixtures :projects, :issues, :time_entries
 
+  def test_spent_time_yields_issue_cumulated_time
+    column = SpentTimeColumn.new
+    assert_equal 154.25, column.value(issues(:issues_001))
+  end
 end
